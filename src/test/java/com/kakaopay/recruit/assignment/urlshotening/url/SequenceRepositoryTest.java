4aeea102f2380336d163d8f84ddf80d82f2e8f47
KaKa(강현식) <cj848@coupang.com>
2018-04-20 00:03:59 +0900

    Repository test 추가

---
+++ src/test/java/com/kakaopay/recruit/assignment/urlshotening/url/SequenceRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.kakaopay.recruit.assignment.urlshotening.url;
+
+import static org.assertj.core.api.Java6Assertions.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SequenceRepositoryTest {
+	@Autowired
+	private SequenceRepository repository;
+
+	@Test
+	public void init_getSequence_test() {
+		repository.initSequence("seq", 0);
+		assertThat(repository.getNextSequenceId("seq")).isEqualTo(1L);
+	}
+}
