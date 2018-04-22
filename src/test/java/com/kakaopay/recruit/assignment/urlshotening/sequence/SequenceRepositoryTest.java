2e1a9563e2a4e367f709fe558b469f9999cc85aa
KaKa(강현식) <cj848@coupang.com>
2018-04-22 16:57:15 +0900

    Package 정리, sequence 관련은 sequence package로 이동

--- src/test/java/com/kakaopay/recruit/assignment/urlshotening/sequence/SequenceRepositoryTest.java
+++ src/test/java/com/kakaopay/recruit/assignment/urlshotening/sequence/SequenceRepositoryTest.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.kakaopay.recruit.assignment.urlshotening.sequence.SequenceRepository;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SequenceRepositoryTest {
